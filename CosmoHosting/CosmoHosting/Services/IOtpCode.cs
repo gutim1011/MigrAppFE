9338aba76eeaec80e69711ac44ae13881f9b0dec
gutim1011 <104235522+gutim1011@users.noreply.github.com>
2025-04-24 00:38:36 -0500

    Delete CosmoHosting directory

--- CosmoHosting/CosmoHosting/Services/IOtpCode.cs
+++
@@ -1,8 +0,0 @@
-namespace CosmoHosting.Services
-{
-    public interface IOtpCode
-    {
-        string GenerateOtp(string secretKey);
-        bool ValidateOtp(string secretKey, string otpCode);
-    }
-}
